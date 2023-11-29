b72a97fec2a7711004910a9a5be832db169fd54a
sandip <sandeep47nepali@gmail.com>
2023-11-29 16:54:45 +0545

    setup database

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_DB_URI)
+  .then(() => {
+    console.log("Connected to Database");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log(`Server is running on port 3000`);
+});

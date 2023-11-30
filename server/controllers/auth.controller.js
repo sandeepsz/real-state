cde3b0e0390717af36a139216f7a84a7404a3d93
sandip <sandeep47nepali@gmail.com>
2023-11-30 12:17:26 +0545

    create middleware and function to handle error

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,16 +1,18 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import errorHandler from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const newHashPassword = await bcryptjs.hash(password, 10);
 
   const newUser = new User({ username, email, password: newHashPassword });
 
-  await newUser.save();
+  try {
+    await newUser.save();
 
-  res.status(200).json({
-    msg: "Signup Success!",
-    user: newUser,
-  });
+    res.status(201).json("User Created Successfully");
+  } catch (error) {
+    next(error);
+  }
 };
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -5,12 +5,12 @@ const userSchema = new mongoose.Schema(
     username: {
       type: String,
       required: true,
-      uniuqe: true,
+      unique: true,
     },
     email: {
       type: String,
       required: true,
-      uniuqe: true,
+      unique: true,
     },
     password: {
       type: String,

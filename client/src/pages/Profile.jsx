5395a28fd4bb0902b9d40b15104b66317259c274
sandip <sandeep47nepali@gmail.com>
2023-12-04 16:14:24 +0545

    create image update functionality sucessfully

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,10 +1,126 @@
+import { useSelector } from "react-redux";
+import { useRef, useState, useEffect } from "react";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
+
 const Profile = () => {
+  const fileRef = useRef(null);
+  const { currentUser, loading } = useSelector((state) => state.user);
+  const [file, setFile] = useState(undefined);
+  const [fileper, setFileper] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+
+  const [formData, setFormData] = useState({});
+
+  console.log(fileper);
+
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file?.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFileper(Math.round(progress));
+      },
+      (error) => {
+        setFileUploadError(true);
+        error.message;
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setFormData({ ...formData, avatar: downloadURL });
+        });
+      }
+    );
+  };
+  // firebase storage
+  // allow read;
+  // allow write: if
+  // request.resource.size < 2 * 1024 * 1024 &&
+  // request.resource.contentType.matches('image/.*')
+
   return (
-    <div>
+    <div className=" px-4 max-w-lg mx-auto">
       <h1 className="text-[24px]  text-center font-semibold mt-2">Profile</h1>
 
-      <form>
-        <input type="text" />
+      <form className="flex flex-col gap-4">
+        <input
+          onChange={(e) => setFile(e.target.files[0])}
+          type="file"
+          ref={fileRef}
+          hidden
+          accept="image/*"
+        />
+
+        <img
+          onClick={() => fileRef.current.click()}
+          src={formData.avatar || currentUser.avatar}
+          alt="avatar"
+          className=" relative h-24 w-24 rounded-full object-cover  mx-auto cursor-pointer my-4"
+        />
+        <p className="text-sm self-center">
+          {fileUploadError ? (
+            <span className="text-red-700">
+              Error Image upload (image must be less than 2 mb);
+            </span>
+          ) : fileper > 0 && fileper < 100 ? (
+            <span className="text-green-700">{`Uploading ${fileper}%`}</span>
+          ) : fileper === 100 ? (
+            <span className="text-green-700">Image successfully uploaded!</span>
+          ) : (
+            ""
+          )}
+        </p>
+
+        <input
+          required
+          type="text"
+          placeholder="username"
+          className="border p-3 rounded-lg focus:outline-none"
+          id="username"
+        />
+        <input
+          required
+          type="text"
+          placeholder="email"
+          className="border p-3 rounded-lg focus:outline-none"
+          id="username"
+        />
+        <input
+          required
+          type="text"
+          placeholder="password"
+          className="border p-3 rounded-lg focus:outline-none"
+          id="username"
+        />
+        <button
+          disabled={loading}
+          className="bg-[#003b36] p-3 text-white  uppercase rounded-lg hover:opacity-90 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Update"}
+        </button>
+        <button
+          disabled={loading}
+          className="bg-green-500 p-3 text-white  uppercase rounded-lg hover:opacity-90 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Create List"}
+        </button>
       </form>
     </div>
   );
